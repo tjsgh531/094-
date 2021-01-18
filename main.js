5d9a2727cc788f4b6cf517a842428cd509b82003
Guest <guest-v1isty@h26817.sgb.skir.kr>
2021-01-18 22:53:32 +0900

    merge_version

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const alertBtn = document.getElementById('alertBtn');
+const buttons = document.querySelectorAll('.Btn');
 
-alertBtn.addEventListener('click',onClick);
+buttons[0].addEventListener('click',onAlert);
+buttons[1].addEventListener('click',onCofirm);
+buttons[2].addEventListener('click',onPrompt);
 
-function onClick(){
-    alert('클릭되었습니다');
+const result = document.getElementById('result');
+
+function onAlert(){
+    alert('<-----경고 창----->');
+}
+function onCofirm(){
+    const isResult = confirm('긍정적인 변화를 위해 필요한 것은 노력하는 것');
+    result.textContent =`${isResult}`;
+}
+function onPrompt(){
+    const isResult = prompt('필적확인란','긍정적인 변화를 위해 필요한 것은 노력하는 것');
+    result.textContent=`${isResult}`;
 }
